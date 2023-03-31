db25971e68051ed0b1a54b18b104aa31df7a40b1
VladislavShults <shvs93@mail.ru>
2023-03-31 14:30:08 +0500

    nodemailer

--- src/infrastructure/SMTP-adapter/email-service.ts
+++ src/infrastructure/SMTP-adapter/email-service.ts
@@ -6,7 +6,7 @@ export class EmailService {
   async sendEmailRecoveryCode(email: string, code: string) {
     const transporter = nodemailer.createTransport({
       service: 'gmail',
-      host: 'smtp.gmail.com',
+      // host: 'smtp.gmail.com',
       auth: {
         user: process.env.userNodemailer,
         pass: process.env.passwordNodemailer,
