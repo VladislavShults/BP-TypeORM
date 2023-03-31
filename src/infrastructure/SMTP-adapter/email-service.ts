47c8280f4b756732682116deffbbf427de130631
VladislavShults <shvs93@mail.ru>
2023-03-31 14:09:23 +0500

    nodemailer

--- src/infrastructure/SMTP-adapter/email-service.ts
+++ src/infrastructure/SMTP-adapter/email-service.ts
@@ -1,10 +1,8 @@
 import { Injectable } from '@nestjs/common';
-import { EmailManager } from './email-manager';
 import * as nodemailer from 'nodemailer';
 
 @Injectable()
 export class EmailService {
-  constructor(private readonly emailManager: EmailManager) {}
   sendEmailRecoveryCode(email: string, code: string) {
     const transporter = nodemailer.createTransport({
       service: 'gmail',
@@ -15,14 +13,27 @@ export class EmailService {
       },
     });
 
-    const info = transporter.sendMail({
-      from: '"Vladislav" <shvs1510@gmail.com>',
-      to: email,
-      subject: 'code',
-      html:
-        "<a href='https://some-front.com/confirm-registration?code=" +
-        code +
-        "'>code</a>",
-    });
+    const sendMessage = async () => {
+      await transporter.sendMail({
+        from: '"Vladislav" <shvs1510@gmail.com>',
+        to: email,
+        subject: 'code',
+        html:
+          "<a href='https://some-front.com/confirm-registration?code=" +
+          code +
+          "'>code</a>",
+      });
+    };
+
+    sendMessage();
+    // const info = transporter.sendMail({
+    //   from: '"Vladislav" <shvs1510@gmail.com>',
+    //   to: email,
+    //   subject: 'code',
+    //   html:
+    //     "<a href='https://some-front.com/confirm-registration?code=" +
+    //     code +
+    //     "'>code</a>",
+    // });
   }
 }
